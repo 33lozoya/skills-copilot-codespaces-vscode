31994c7f3bc7cb683467f160fa2ca2d1bb764d02
María Lozoya <131869743+33lozoya@users.noreply.github.com>
2023-10-18 14:42:26 +0000

    Copilot third commit

---
+++ comment.js
@@ -0,0 +1,21 @@
+// Create web server with express
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Use static file
+app.use(express.static('public'));
+
+// Use body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Use mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/comment', { useNewUrlParser: true });
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const commentSchema = new Schema({
+    content: String,
